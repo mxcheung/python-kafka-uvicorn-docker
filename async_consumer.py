84434381f18cfe98b6b13c95e302039827b13db3
mxcheung <mxcheung@users.noreply.github.com>
2023-03-11 11:31:10 +1100

    Update async_consumer.py

--- async_consumer.py
+++ async_consumer.py
@@ -77,6 +77,12 @@ async def to_thread(func, /, *args, **kwargs):
     func_call = functools.partial(ctx.run, func, *args, **kwargs)
     return await loop.run_in_executor(None, func_call)
 
+async def to_thread(func, /, *args, **kwargs):
+    loop = asyncio.get_running_loop()
+    ctx = contextvars.copy_context()
+    func_call = functools.partial(ctx.run, func, *args, **kwargs)
+    return await loop.run_in_executor(None, func_call)
+
 def kafka_consumer():
     global consumer, producer
     # Instantiate
